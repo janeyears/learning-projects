ec0f51b001f744bb2d39b6d5bf63c0f794a595b1
Jane Years <janeletoxx@gmail.com>
2022-09-20 10:11:04 +0300

    new project

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -56,6 +56,6 @@ app.post("/work", function (req, res) {
 app.get("/about", function (req, res) {
     res.render("about");
 });
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Sever is running on port 3000");
 });
